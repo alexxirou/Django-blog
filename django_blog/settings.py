f15c7e6e8e1bca383b3f02f4fa39d5d95472f44f
Alexandros Xirouchakis <Alex_xirou@hotmail.com>
2021-03-08 19:26:04 +0100

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/'
+STATIC_ROOT = ''
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE =  'whitenoise.storage.CompressedStaticFilesStorage'
