2611f09d965721611ada74027432bbae9888df98
Alexandros Xirouchakis <Alex_xirou@hotmail.com>
2021-03-08 16:11:38 +0100

    Update urls.py

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 # Use include() to add paths from the catalog application 
-from django.urls import include
 
 urlpatterns += [
     path('about/', include('about.urls')),
@@ -61,8 +62,6 @@ urlpatterns += [
     path('', RedirectView.as_view(url='about/', permanent=True)),
 ]
 # Use static() to add url mapping to serve static files during development (only)
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
